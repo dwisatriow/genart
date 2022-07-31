c057c8ba289ce47356951839c24b58383d087cb5
dwisatriow <dwisatrioow@gmail.com>
2022-07-31 23:05:53 +0700

    Canvas api basics

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require('canvas-sketch')
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
-};
+  dimensions: [2048, 2048],
+}
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
-  };
-};
+    context.fillStyle = 'skyblue'
+    context.fillRect(0, 0, width, height)
 
-canvasSketch(sketch, settings);
+    context.beginPath()
+    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false)
+    context.fillStyle = 'gray'
+    context.fill()
+
+    context.lineWidth = 10
+    context.stroke()
+  }
+}
+
+canvasSketch(sketch, settings)
